d1f3d48e7cc838dd91476a5ad4463a7abc18e7af
gewyckler <gewyckler@gmail.com>
2019-10-22 20:55:04 +0200

    Add Updated config-server

--- src/main/java/pl/javagda25/cloud/configserver/ConfigServerApplication.java
+++ src/main/java/pl/javagda25/cloud/configserver/ConfigServerApplication.java
@@ -2,11 +2,12 @@ package pl.javagda25.cloud.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 
 @EnableConfigServer
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
